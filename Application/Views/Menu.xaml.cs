dcc1e3fd4f1b87450c376bc3e37ae814016c7c79
itschip <onlygamernerd@gmail.com>
2022-01-19 00:06:57 +0100

    feat(vehicles): initial progress"

--- Application/Views/Menu.xaml.cs
+++ Application/Views/Menu.xaml.cs
@@ -4,6 +4,8 @@ namespace ToolKitV.Views
 {
     public partial class Menu : UserControl
     {
+
+        public static int tabValue = 1;
         public Menu()
         {
             InitializeComponent();
